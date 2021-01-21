830af567b8228a7708d787a3bcb4cf388199e65a
jimkiarie8@gmail.com <jimmy@localhost.localdomain>
2021-01-21 10:24:11 +0300

    commit

---
+++ resources/views/emails/welcome.blade.php
@@ -0,0 +1,4 @@
+Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint nihil commodi similique ducimus ea. Itaque debitis odio earum distinctio, eos explicabo quasi excepturi nostrum! Commodi ut itaque modi id illum.
+
+
+New Subscription Webhook
